7d282aee42170f5fd1b719654912cda6bcd0f2f6
mollycann <mollycann4567@gmail.com>
2018-08-06 19:11:11 +0000

    automatically backed up by learn

--- user_imput.rb
+++ user_imput.rb
@@ -1,3 +1,6 @@
 puts "What's your name"
 name = gets.chomp 
 puts "My name is #{name} and you're watching Disney Channel"
+puts "What's your favorite channel?"
+channel = gets.chop 
+puts "My name is #{name} and you're watching #{channel}"
